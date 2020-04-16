a635b4de3c7b2503489df8a05e8a7f6468a2d7ff
JacobWeatherly <63430739+JacobWeatherly@users.noreply.github.com>
2020-04-16 23:23:37 +0000

    MySecondProject Week 13

---
+++ MySecondProject/App copy 2.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+export default function MyApp() {
+
+  function getFullName (fName, mName, lName){
+    return fName + " " + mName + " " + lName;
+
+  }
+
+  const pet = "Dog";
+  return (
+  
+      <Text>
+        {"\n\n\n\n\n\n\n\n\n"}
+        Hello, I am a student in CIS 340!{"\n"}
+        My full name is {getFullName("Jaocb", "Paul", "Weatherly")}{"\n"}
+        I have a {pet}!
+        </Text>
+
+        
+  
+  );
+}
+
+
